9f8175203f3fd8ea5b3ef517d33be5de507f7776
MelissaSuapi <pine316.msp@gmail.com>
2025-06-03 21:35:55 -0500

    cambios10

--- src/main/java/com/cesde/proyecto_integrador/dto/HorariosDTO.java
+++ src/main/java/com/cesde/proyecto_integrador/dto/HorariosDTO.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 @Data
 public class HorariosDTO {
-    private String dia;
-    private String horaInicio;
-    private String horaFin;
     private Long idClases;
     private Long idProfesor;
     private Long idEstudiante;
